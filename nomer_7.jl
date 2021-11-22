2bbd7fbde457728fc0788dbc9f110a5e1d0f0404
Romanjo28 <90093475+Romanjo28@users.noreply.github.com>
2021-11-22 09:40:12 +0300

    Update nomer_7.jl

--- nomer_7.jl
+++ nomer_7.jl
@@ -1,10 +1,11 @@
-using HorizonSideRobots
+using HorizonSideRobots 
+include("lib_for_practic_10.jl")
 
-function q(r::Robot, side::HorizonSide)
+function cherez_2(r::Robot, side::HorizonSide)
     while !(isborder(r, side))
         putmarker!(r)
-        move!(r, side)
-        move!(r,side)
+        try_move!(r, side)
+        try_move!(r,side)
     end
 end
 
@@ -12,7 +13,7 @@ function step_verh(r::Robot, side::HorizonSide)
     if ismarker(r)==false
         move!(r, Nord)
     end
-    
+
     if ismarker(r)==true
         move!(r, Nord)
         move!(r, side)
@@ -21,10 +22,10 @@ end
 
 function mark_2(r::Robot) 
     while (isborder(r, Nord)==false && isborder(r, Ost)==false) 
-        function q(r, Ost)
-        function step_verh(r, West)
-        function q(r, West)
-        function step_verh(r, Ost)
+        cherez_2(r, Ost)
+        step_verh(r, West)
+        cherez_2(r, West)
+        step_verh(r, Ost)
     end
 end
 
